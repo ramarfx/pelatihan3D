940dd6917c9b8d6cf7ff812a724a7a9bd0da3004
ramarfx <ramagamiing33@gmail.com>
2024-01-25 12:33:12 +0700

    jadiiiii kotakkkk

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 //canvas
 const canvas = document.getElementById('canvas')
@@ -12,3 +13,43 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const scene = new THREE.Scene()
+
+
+//object dan material
+const boxGeometry = new THREE.BoxGeometry(10, 1, 10)
+const boxMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+})
+
+//menyatukan box elemen dengan material
+const box = new THREE.Mesh(boxGeometry, boxMaterial)
+scene.add(box)
+
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+//untuk menampilkan grid pada layar
+const gridHelper = new THREE.GridHelper(30)
+scene.add(gridHelper)
+
+//membuat kamera
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+)
+camera.position.set(-10, 10, 30)
+
+//untuk orbit
+const orbit = new OrbitControls(camera, canvas)
+
+//update posisi orbit
+orbit.update();
+
+
+function animate() {
+    renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate)
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     root: 'src/',
     publicDir: '../static/',
-    base: '/',
+    base: './',
     server: {
         host: true
     },
